b4679f22c13d69399d0574b9e8ffa7aa670ddf69
William Jenkins <wfjenkins@ucsd.edu>
2025-06-13 23:10:47 -0700

    Extend plotting for 3DVHA/all.

--- scripts/plot/plot_shru.py
+++ scripts/plot/plot_shru.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-"""Plot SHRU data.
+"""Plot acoustic data (time series & spectrograms) from 3DVHA, VLA1, and VLA2.
 
-Suggested times:
+Times of interest:
+3DVHA:
 VLA 1: 2023-12-01T21:26:00 to 2023-12-02T01:13:00
 VLA 2: 2023-12-01T21:44:00 to 2023-12-02T01:31:00
 Fin whale: 2023-12-01T21:26:00 to 2023-12-01T23:19:00
@@ -10,6 +11,8 @@ Fin whale: 2023-12-01T21:26:00 to 2023-12-01T23:19:00
 from argparse import ArgumentParser
 from collections.abc import Sequence
 import logging
+from pathlib import Path
+from typing import Callable
 
 import dotenv
 import matplotlib.pyplot as plt
@@ -19,17 +22,22 @@ from tritonoa.data.time import TIME_PRECISION, convert_datetime64_to_string
 from tritonoa.data.reader import read_inventory
 
 from vineyard.config import get_path
-from vineyard.plotting import plot_shru_pectrograms, savefig_kwargs
+from vineyard.plotting import (
+    plot_3dvha_spectrograms,
+    plot_shru_spectrograms,
+    savefig_kwargs,
+)
 
 dotenv.load_dotenv()
 
+SENSORS = ["3dvha", "vla1", "vla2"]
 nperseg = 256
 STFT_PARAMS = {
     "nperseg": nperseg,
     "hop": int(0.05 * nperseg),
     "nfft": 2**12,
-    "fmin": 10.0,
-    "fmax": 50.0,
+    "fmin": 0.0,
+    "fmax": 250.0,
 }
 
 
@@ -44,7 +52,7 @@ def condition_data(
 
     ds_filt = ds.copy()
     if target_fs is not None:
-        dec_factor = int(np.round(ds_filt.fs / target_fs))
+        dec_factor = int(np.round(ds_filt.stats.sampling_rate / target_fs))
         ds_filt.decimate(dec_factor)
     if filt_type is not None and filt_freq is not None:
         ds_filt.filter(
@@ -54,52 +62,45 @@ def condition_data(
     return ds_filt
 
 
-def main(
-    sensor: str,
-    start: str,
-    end: str,
-    multi: bool,
-    interval: float,
-    savefig: bool,
-    target_fs: float,
-) -> None:
+def dataloader_3dvha(
+    inventory: Path,
+    target_fs: float | None = None,
+    filt_type: str | None = None,
+    filt_freq: float | Sequence[float] | None = None,
+):
+    def dataloader(start_time: np.datetime64, end_time: np.datetime64) -> DataStream:
+        ds_raw = condition_data(
+            read_inventory(inventory, start_time, end_time),
+            target_fs=target_fs,
+            filt_type=filt_type,
+            filt_freq=filt_freq,
+        )
+        ds = ds_raw.copy()
+        ds.data[0:3] = -ds_raw.data[0:3]
+        return ds
 
-    inv = get_path(f"{sensor}_inventory")
-    start = np.datetime64(start, TIME_PRECISION)
-    end = np.datetime64(end, TIME_PRECISION)
+    return dataloader
 
-    if multi:
-        interval = np.timedelta64(interval, "s")
-    else:
-        interval = end - start
 
-    for start_time in np.arange(start, end, interval):
-        end_time = start_time + interval
-        logging.info(f"Plotting {sensor.upper()} data from {start_time} to {end_time}")
-        ds = condition_data(
-            read_inventory(inv, start_time, end_time), target_fs=target_fs
+def dataloader_all_sensors():
+    return
+
+
+def dataloader_shru(
+    inventory: Path,
+    target_fs: float | None = None,
+    filt_type: str | None = None,
+    filt_freq: float | Sequence[float] | None = None,
+) -> Callable:
+    def dataloader(start_time: np.datetime64, end_time: np.datetime64) -> DataStream:
+        return condition_data(
+            read_inventory(inventory, start_time, end_time),
+            target_fs=target_fs,
+            filt_type=filt_type,
+            filt_freq=filt_freq,
         )
 
-        start_str = convert_datetime64_to_string(start_time)
-        end_str = convert_datetime64_to_string(end_time)
-        start_str_read = convert_datetime64_to_string(start_time, readable=True)
-        end_str_read = convert_datetime64_to_string(end_time, readable=True)
-
-        fig = plot_shru_pectrograms(ds, **STFT_PARAMS)
-        fig.suptitle(
-            f"{sensor.upper()} Data from {start_str_read}Z to {end_str_read}Z",
-            fontsize=12,
-        )
-        if savefig:
-            fname = f"{sensor}_{start_str}-{end_str}.png"
-            fig.savefig(get_path(f"{sensor}_data_view") / fname, **savefig_kwargs)
-            plt.close(fig)
-            continue
-        if multi:
-            plt.draw()
-
-    if not savefig:
-        plt.show()
+    return dataloader
 
 
 def setup_parser() -> ArgumentParser:
@@ -107,28 +108,28 @@ def setup_parser() -> ArgumentParser:
     parser.add_argument(
         "--sensor",
         type=str,
-        default="vla1",
-        choices=["vla1", "vla2"],
+        default="3dvha",
+        choices=SENSORS + ["all"],
         help="Sensor to plot data for. Default is 'vla1'.",
     )
     parser.add_argument(
         "--start",
         type=str,
-        default="2023-12-01T21:44:00",
-        # default="2023-12-01T22:25:00",
+        # default="2023-12-01T21:44:00",
+        default="2023-12-01T22:25:00",
         help="Start time of the data to extract.",
     )
     parser.add_argument(
         "--end",
         type=str,
-        default="2023-12-02T01:31:00",
-        # default="2023-12-01T22:26:00",
+        # default="2023-12-02T01:31:00",
+        default="2023-12-01T22:26:00",
         help="End time of the data to extract.",
     )
     parser.add_argument(
         "--multi",
         action="store_true",
-        default=True,
+        default=False,
         help="Generate multiple figures with a specified interval.",
     )
     parser.add_argument(
@@ -145,11 +146,109 @@ def setup_parser() -> ArgumentParser:
     )
     parser.add_argument(
         "--target_fs",
-        type=int,
-        default=40,
+        type=float,
+        default=1000.0,
         help="Decimation factor for the data.",
     )
+    parser.add_argument(
+        "--filter",
+        type=str,
+        default="highpass",
+        choices=["lowpass", "highpass", "bandpass", None],
+        help="Filter type to apply to the data.",
+    )
+    parser.add_argument(
+        "--filt_freq",
+        type=float,
+        nargs="+",
+        default=10.0,
+        help="Frequency or frequencies for the filter.",
+    )
     return parser
+
+
+def setup_plotting(
+    sensor: str,
+    target_fs: float | None = None,
+    filt_type: str | None = None,
+    filt_freq: float | Sequence[float] | None = None,
+) -> tuple[Callable, Callable]:
+    match sensor:
+        case "all":
+            invs = [get_path(f"{s}_inventory") for s in SENSORS]
+            [
+                logging.info(f"Using inventory for {sensor.upper()}: {inv.resolve()}")
+                for sensor, inv in zip(SENSORS, invs)
+            ]
+        case "3dvha":
+            inv = get_path(f"{sensor}_inventory")
+            plotter = plot_3dvha_spectrograms
+            dataloader = dataloader_3dvha(
+                inv, target_fs=target_fs, filt_type=filt_type, filt_freq=filt_freq
+            )
+            logging.info(f"Using inventory for {sensor.upper()}: {inv.resolve()}")
+        case "vla1" | "vla2":
+            inv = get_path(f"{sensor}_inventory")
+            plotter = plot_shru_spectrograms
+            dataloader = dataloader_shru(
+                inv, target_fs=target_fs, filt_type=filt_type, filt_freq=filt_freq
+            )
+            logging.info(f"Using inventory for {sensor.upper()}: {inv.resolve()}")
+    return plotter, dataloader
+
+
+def main(
+    sensor: str,
+    start: str,
+    end: str,
+    multi: bool,
+    interval: float,
+    savefig: bool,
+    target_fs: float | None = None,
+    filt_type: str | None = None,
+    filt_freq: float | Sequence[float] | None = None,
+) -> None:
+
+    plotter, dataloader = setup_plotting(
+        sensor,
+        target_fs=target_fs,
+        filt_type=filt_type,
+        filt_freq=filt_freq,
+    )
+
+    start = np.datetime64(start, TIME_PRECISION)
+    end = np.datetime64(end, TIME_PRECISION)
+
+    if multi:
+        interval = np.timedelta64(interval, "s")
+    else:
+        interval = end - start
+
+    for start_time in np.arange(start, end, interval):
+        end_time = start_time + interval
+        logging.info(f"Plotting {sensor.upper()} data from {start_time} to {end_time}")
+        ds = dataloader(start_time, end_time)
+        fig = plotter(ds, **STFT_PARAMS)
+
+        start_str = convert_datetime64_to_string(start_time)
+        end_str = convert_datetime64_to_string(end_time)
+        start_str_read = convert_datetime64_to_string(start_time, readable=True)
+        end_str_read = convert_datetime64_to_string(end_time, readable=True)
+
+        fig.suptitle(
+            f"{sensor.upper()} Data from {start_str_read}Z to {end_str_read}Z",
+            fontsize=12,
+        )
+        if savefig:
+            fname = f"{sensor}_{start_str}-{end_str}.png"
+            fig.savefig(get_path(f"{sensor}_data_view") / fname, **savefig_kwargs)
+            plt.close(fig)
+            continue
+        if multi:
+            plt.draw()
+
+    if not savefig:
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -162,5 +261,7 @@ if __name__ == "__main__":
         args.multi,
         args.interval,
         args.savefig,
-        args.target_fs,
+        target_fs=args.target_fs,
+        filt_type=args.filter,
+        filt_freq=args.filt_freq,
     )
