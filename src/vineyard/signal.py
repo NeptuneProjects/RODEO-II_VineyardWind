7c896f4a48e91bd1093cfa5c1f583a2916ea3962
William Jenkins <wfjenkins@ucsd.edu>
2025-06-17 05:46:25 -0700

    Add function to resample data streams.

--- src/vineyard/signal.py
+++ src/vineyard/signal.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 """Module for common signal processing functions used in the project."""
-
-from dataclasses import dataclass
 
 import numpy as np
 import numpy.typing as npt
+from tritonoa.data.stream import DataStream
+from tritonoa.signal.util import resample_ratio
 
 
 def convert_to_strain_rate(
@@ -18,6 +17,19 @@ def convert_to_strain_rate(
         scale_factor * calibration_factor * sampling_rate_hz / gauge_length_m
     )
     return data * conversion_factor
+
+
+def resample_datastreams(data: list[DataStream], target_fs: float) -> list[DataStream]:
+    if isinstance(data, DataStream):
+        data = [data]
+
+    resampled_data = []
+    for ds in data:
+        fs = ds.stats.sampling_rate
+        p, q = resample_ratio(fs, target_fs)
+        resampled_data.append(ds.filter("highpass", target_fs / 2).resample_poly(p, q))
+
+    return resampled_data
 
 
 def subtract_median(data: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
