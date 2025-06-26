efb1ff4f3a84962ccb1f0135008455600b088ec0
William Jenkins <wfjenkins@ucsd.edu>
2025-06-26 07:42:15 -0700

    Reorder & remove unused.

--- scripts/process/strikes_corr.py
+++ scripts/process/strikes_corr.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Compute correlation coefficients between all signal pairs for each station."""
 
 import argparse
+from collections.abc import Sequence
 from dataclasses import dataclass
 import logging
 import os
@@ -9,242 +11,199 @@ from pathlib import Path
 import dotenv
 import h5py
 import numpy as np
-import polars as pl
+from numpy.typing import NDArray
 from tqdm import tqdm
-from tritonoa.data.stream import DataStream
-from tritonoa.data.time import TIME_PRECISION
+from tritonoa.data.reader import read_hdf5_group
 
-from rodeo.detect import parse_detection
 from rodeo.utils import (
     compute_array_size,
-    decimation_factor,
     initialize_julia,
     logging_kwargs,
 )
 from vineyard.config import get_path
+from vineyard.signal import process_datastream
 
 dotenv.load_dotenv()
-logging.basicConfig(**logging_kwargs)
 
 
 @dataclass
 class Record:
-    event: str
-    station: str
-    sampling_rate: float
+    sensor: str
     time_diff: np.ndarray
     corr: np.ndarray
-    etime: np.ndarray
 
     def save_h5(self, path: Path) -> None:
         """Save the record to an HDF5 file.
 
         The record is saved to the output file in the following format:
         ```
-        /station
+        /sensor
             /time_diff
             /corr
-            /etime
         ```
-        `time_diff` and `corr` are 3D arrays with the shape
-        `(num_channels, num_detections, num_detections)`.
+        `time_diff` and `corr` are 2D arrays with the shape
+        `(num_detections, num_detections)`.
 
         Args:
             path: Path to the output HDF5 file.
         """
         with h5py.File(path, "a") as file:
-            # Set "event" attr if not already set:
-            if "event" not in file.attrs:
-                file.attrs["event"] = self.event
-
             # Create a new group for the station if it doesn't exist:
-            if self.station not in file:
-                file.create_group(self.station)
+            if self.sensor not in file:
+                file.create_group(self.sensor)
 
             # Save the data:
-            grp = file[f"{self.station}"]
-            grp.attrs["event"] = self.event
-            grp.attrs["station"] = self.station
-            grp.attrs["sampling_rate"] = self.sampling_rate
+            grp = file[f"{self.sensor}"]
+            grp.attrs["sensor"] = self.sensor
             grp.create_dataset("time_diff", data=self.time_diff)
             grp.create_dataset("corr", data=self.corr)
-            grp.create_dataset("etime", data=self.etime)
 
 
-def condition_signal(stream: DataStream, target_fs: float) -> tuple[DataStream, int]:
-    """Condition the signal by decimating and filtering it.
+def process_sensor(
+    sensor: str, sensor_group: h5py.Group, sp_kwargs: dict = {}
+) -> Record:
+    data, t0 = retrieve_data(sensor_group, **sp_kwargs)
 
-    Args:
-        stream: Data stream.
-        target_fs: Target sampling rate for the signal.
+    num_detections = data.shape[0]
+    time_diff = np.full((num_detections, num_detections), np.nan)
+    max_corr = np.full((num_detections, num_detections), np.nan)
 
-    Returns:
-        Decimated and filtered data stream.
-        Decimation factor.
-    """
-    dec_factor = decimation_factor(stream.stats.sampling_rate, target_fs)
-    ds = stream.decimate(dec_factor)
-    ds.filter("highpass", freq=10.0)
-    return ds, dec_factor
-
-
-def data_shape(
-    dbfile: Path,
-) -> tuple[int, int]:
-    """Return shape of data within the HDF file.
-
-    Args:
-        dbfile: Path to the HDF5 file.
-
-    Returns:
-        Number of channels and number of detections.
-    """
-    channels = []
-    num_detections = []
-    num_samples = None
-    with h5py.File(dbfile, "r") as file:
-        logging.info(f"Loaded {dbfile}")
-        for chan_group in file.values():
-            channels.append(int(chan_group.name[-1]))
-            num_detections.append(len(chan_group))
-            if num_samples is None:
-                num_samples = next(iter(chan_group.values()))["waveform"].shape[1]
-
-    return len(channels), max(num_detections)
-
-
-def retrieve_data(dbpath: Path, target_fs: float) -> np.ndarray:
-    """Retrieve data from the HDF5 file.
-
-    Recursively loads data from the HDF5 file into a NumPy array.
-
-    Args:
-        dbpath: Path to the HDF5 file.
-        target_fs: Target sampling rate for the signals.
-
-    Returns:
-        Array containing the signals.
-        Array containing the initial times of the signals.
-        Sampling rate of the signals.
-    """
-    num_channels, num_detections = data_shape(dbpath)
-
-    data = None
-    t0 = np.full((num_channels, num_detections), np.nan)
-
-    with h5py.File(dbpath, "r") as file:
-        for i, chan_group in enumerate(file.values()):
-            logging.info(f"Loading channel {i}.")
-            for j, det_group in tqdm(
-                enumerate(chan_group.values()), total=num_detections
-            ):
-                ds, _ = condition_signal(
-                    DataStream(
-                        stats=parse_detection(det_group),
-                        data=det_group["waveform"][:],
-                    ),
-                    target_fs=target_fs,
-                )
-                if data is None:
-                    # Initialize within loop since num_samples depends on target_fs.
-                    data = np.full(
-                        (num_channels, num_detections, ds.num_samples), np.nan
-                    )
-
-                data[i, j] = ds.data.squeeze()
-                t0[i, j] = ds.stats.time_init
-
-    return data, t0, ds.stats.sampling_rate
-
-
-def main(index: Path, output: Path) -> None:
-    # jl = initialize_julia("CrossCorr")
-
-    df = pl.read_csv(index).with_columns(
-        pl.col("time").str.to_datetime(time_unit=TIME_PRECISION)
+    logging.info(f"Data shape: {num_detections} detections.")
+    logging.info(
+        f"Size of arrays: {compute_array_size([max_corr, time_diff]) / (1024 ** 3):.2f} GB."
     )
 
-    for sensor, channel, strike_index, time in df.iter_rows():
-        print(sensor, channel, strike_index, time)
+    jl = initialize_julia("CrossCorr")
+    threads = jl.seval("Threads.nthreads()")
+    logging.info(f"Number of Julia threads: {threads}.")
 
-#     for dbfile in dbfiles:
-#         station_name = "-".join(dbfile.stem.split("_")[1:])
-#         logging.info(f"Processing station {station_name}.")
+    jl_data = jl.seval("x -> Matrix{Float64}(x)")(data)
+    jl_time = jl.seval("x -> Vector{Float64}(x)")(t0)
 
-#         data, t0, target_fs = retrieve_data(dbfile, args.target_fs)
-#         num_channels = data.shape[0]
-#         num_detections = data.shape[1]
-#         time_diff = np.full((num_channels, num_detections, num_detections), np.nan)
-#         max_corr = np.full((num_channels, num_detections, num_detections), np.nan)
-#         etime = np.full((num_channels, num_detections, num_detections), np.nan)
-#         logging.info(
-#             f"Data shape: {num_channels} channels; {num_detections} detections."
-#         )
-#         logging.info(
-#             f"Size of arrays: {compute_array_size([max_corr, etime, time_diff]) / (1024 ** 3):.2f} GB."
-#         )
+    time_diff = np.array(jl.CrossCorr.dt_matrix(jl_time))
+    max_corr = np.array(jl.CrossCorr.corr_matrix(jl_data))
 
-#         threads = jl.seval("Threads.nthreads()")
-#         logging.info(f"Number of Julia threads: {threads}.")
+    logging.info(f"Computed time_diff and max_corr for sensor {sensor.upper()}.")
+    logging.info(f"Shape of time_diff: {time_diff.shape}, max_corr: {max_corr.shape}.")
 
-#         for i in range(num_channels):
-#             jl_data = jl.seval("x -> Matrix{Float64}(x)")(data[i])
-#             jl_time = jl.seval("x -> Vector{Float64}(x)")(t0[i])
+    return Record(
+        sensor=sensor,
+        time_diff=time_diff,
+        corr=max_corr,
+    )
 
-#             # logging.info(f"Computing dt for channel {i}.")
-#             # time_diff[i] = np.array(jl.CrossCorr.dt_matrix(jl_time))
-#             # logging.info(f"Computed dt for channel {i}.")
 
-#             logging.info(f"Computing corr & etime for channel {i}.")
-#             max_corr_mat, etime_mat = jl.CrossCorr.corr_matrix(jl_data, target_fs)
-#             max_corr[i] = np.array(max_corr_mat)
-#             etime[i] = np.array(etime_mat)
-#             logging.info(f"Computed corr & etime for channel {i}.")
+def retrieve_data(sensor_group: h5py.Group, **kwargs) -> tuple[NDArray, NDArray]:
+    num_detections = len(sensor_group)
 
-#         record = Record(
-#             event=event_name,
-#             station=station_name,
-#             sampling_rate=target_fs,
-#             time_diff=time_diff,
-#             corr=max_corr,
-#             etime=etime,
-#         )
+    data = None
+    t0 = np.full((num_detections,), np.nan)
+    for i, strike_group in tqdm(
+        enumerate(sensor_group.values()), desc="Loading data", total=num_detections
+    ):
+        ds = process_datastream(read_hdf5_group(strike_group), **kwargs)
+        # Initialize within loop since num_samples depends on target_fs:
+        if data is None:
+            data = np.full((num_detections, ds.num_samples), np.nan)
 
-#         record.save_h5(args.output / f"maxcorr_{event_name}.h5")
-#         logging.info(
-#             f"Saved results to {args.output / f'maxcorr_{event_name}.h5'}."
-#         )
+        if ds.num_samples < data.shape[1]:
+            ds.data = np.pad(ds.data, ((0, 0), (0, data.shape[1] - ds.num_samples)))
+        elif ds.num_samples > data.shape[1]:
+            ds.data = ds.data[:, : data.shape[1]]
+
+        data[i] = ds.data.squeeze()
+        t0[i] = ds.stats.time_init
+
+    return data, t0
+
+
+def main(
+    strike_db: Path,
+    output: Path,
+    detrend: bool,
+    taper_pc: float,
+    dec_factor: int,
+    filt_type: str,
+    filt_freq: float | Sequence[float],
+) -> None:
+    sp_kwargs = {
+        "detrend": detrend,
+        "taper_pc": taper_pc,
+        "dec_factor": dec_factor,
+        "filt_type": filt_type,
+        "filt_freq": filt_freq,
+    }
+    with h5py.File(strike_db, "r") as file:
+        for sensor, sensor_group in file.items():
+            logging.info(f"Processing sensor {sensor.upper()}.")
+            record = process_sensor(sensor, sensor_group, sp_kwargs)
+            record.save_h5(output)
+            logging.info(f"Processed sensor {sensor.upper()}.")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(**logging_kwargs)
     parser = argparse.ArgumentParser(
         description="Compute the coherence between two signals."
     )
     parser.add_argument(
-        "--index",
+        "--db",
         type=Path,
-        default=get_path("strike_index"),
+        default=get_path("strike_data"),
         help="Path to the input HDF5 file containing the signals.",
     )
-    # parser.add_argument(
-    #     "--target_fs",
-    #     type=float,
-    #     help="Target sampling rate for the signals.",
-    #     default=1000.0,
-    # )
-    # parser.add_argument(
-    #     "--max_workers",
-    #     type=int,
-    #     help="Number of workers to use for parallel processing.",
-    #     default=4,
-    # )
+    parser.add_argument(
+        "--max-workers",
+        type=int,
+        help="Number of workers to use for parallel processing.",
+        default=12,
+    )
     parser.add_argument(
         "--output",
         type=Path,
         help="Path to the output HDF5 file to save the results.",
         default=get_path("strike_corr"),
     )
+    parser.add_argument(
+        "--detrend",
+        action="store_true",
+        default=False,
+        help="Apply detrending to the signals before processing.",
+    )
+    parser.add_argument(
+        "--taper_pc",
+        type=float,
+        default=None,
+        help="Percentage of the signal to taper at the beginning and end.",
+    )
+    parser.add_argument(
+        "--dec_factor",
+        type=int,
+        default=None,
+        help="Decimation factor to apply to the signals.",
+    )
+    parser.add_argument(
+        "--filt_type",
+        type=str,
+        default="bandpass",
+        help="Type of filter to apply to the signals.",
+    )
+    parser.add_argument(
+        "--filt_freq",
+        type=float,
+        nargs="+",
+        default=[15.0, 35.0],
+        help="Frequency range for the filter. Provide two values for bandpass filter.",
+    )
     args = parser.parse_args()
-    # os.environ["JULIA_NUM_THREADS"] = str(args.max_workers)
-    # os.environ["PYTHON_JULIACALL_HANDLE_SIGNALS"] = "yes"
-    main(args.index, args.output)
+    os.environ["JULIA_NUM_THREADS"] = str(args.max_workers)
+    os.environ["PYTHON_JULIACALL_HANDLE_SIGNALS"] = "yes"
+    main(
+        args.db,
+        args.output,
+        args.detrend,
+        args.taper_pc,
+        args.dec_factor,
+        args.filt_type,
+        args.filt_freq,
+    )
