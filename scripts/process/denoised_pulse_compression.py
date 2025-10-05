d2abf19ad3fa966379f4bdd9dac32008882dc3cd
William Jenkins <wfjenkins@ucsd.edu>
2025-10-04 19:38:27 -0700

    Add pulse compression script.

--- scripts/process/denoised_pulse_compression.py
+++ scripts/process/denoised_pulse_compression.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt
-from tritonoa.data.reader import read_hdf5
-from tritonoa.data.stream import DataStream
+from pathlib import Path
+
+import h5py
+import numpy as np
+from tritonoa.data.reader import read_hdf5, read_hdf5_group
 
 from vineyard.config import get_path
 
 
-def main():
+def main(denoised_data_path: Path, whale_templates_path: Path, output_path: Path):
     sensors = ["3dvha", "vla1", "vla2"]
-    savepath = get_path("figures") / "denoised"
-    savepath.mkdir(parents=True, exist_ok=True)
+    output_path.mkdir(parents=True, exist_ok=True)
 
-    # TODO: Load whale templates
-    template = None
+    with h5py.File(whale_templates_path, "r") as f_whale:
+        for sensor in sensors:
+            ds = read_hdf5(denoised_data_path / f"{sensor}.h5")
+            ds.data = ds.data[1] / np.max(np.abs(ds.data[1]))
 
-    for sensor in sensors:
-        ds = read_hdf5(get_path("denoised_data") / f"{sensor}.h5")
-        ds.data = ds.data[1]
+            template = read_hdf5_group(f_whale[f"{sensor}_fin_whale"])
+            ds_pc = ds.copy().pulse_compression(template.data.squeeze())
+            ds_pc.data = ds_pc.data / np.max(np.abs(ds_pc.data))
 
-        ds_pc = ds.pulse_compression(template)
+            ds_pc.write_hdf5(output_path / f"{sensor}_pc.h5")
 
 
 if __name__ == "__main__":
-    main()
+    denoised_data_path = get_path("denoised_data")
+    whale_templates_path = get_path("whale_templates")
+    output_path = get_path("pulse_comp_data")
+    main(denoised_data_path, whale_templates_path, output_path)
