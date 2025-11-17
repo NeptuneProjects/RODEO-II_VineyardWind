ca93f4a0988cbea0478cdffc6a0477ac516c5a0b
William Jenkins <wfjenkins@ucsd.edu>
2025-11-16 19:15:33 -0800

    fix typo.

--- scripts/setup/get_data.sh
+++ scripts/setup/get_data.sh
@@ -11,7 +11,7 @@ cp /project/users/ytlin/VW_DAS/SHRUs/VLA1_shru905/12011930.D23 "$VLA1_DIR"
 cp /project/users/ytlin/VW_DAS/SHRUs/VLA1_shru905/12012125.D23 "$VLA1_DIR"
 cp /project/users/ytlin/VW_DAS/SHRUs/VLA1_shru905/12012319.D23 "$VLA1_DIR"
 
-VLA2_DIR="./data/acoustic/raw/VLA2_shru92"
+VLA2_DIR="./data/acoustic/raw/VLA2_shru920"
 mkdir -p "$VLA2_DIR"
 cp /project/users/ytlin/VW_DAS/SHRUs/VLA2_shru920/12011948.D23 "$VLA2_DIR"
 cp /project/users/ytlin/VW_DAS/SHRUs/VLA2_shru920/12012143.D23 "$VLA2_DIR"
