670c52bc305ee34189fe6eadec68923bfd144609
William Jenkins <wfjenkins@ucsd.edu>
2025-11-17 07:49:11 -0800

    Add optional args.

--- scripts/process/run_workflow.sh
+++ scripts/process/run_workflow.sh
@@ -1,5 +1,5 @@
 python scripts/setup/acoustic_inventory.py
 python scripts/process/strikes_find.py
 python scripts/process/strikes_save.py
-python scripts/process/strikes_corr.py
-python scripts/process/template_extraction.py
+python scripts/process/strikes_corr.py --max-workers 256
+python scripts/process/template_extraction.py --save-plots
