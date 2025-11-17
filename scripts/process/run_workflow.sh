710e7c0f127db3167a9ff7cb15cbf6d2c6e3a832
William Jenkins <wfjenkins@ucsd.edu>
2025-11-17 07:47:49 -0800

    Add workflow runner script.

---
+++ scripts/process/run_workflow.sh
@@ -0,0 +1,5 @@
+python scripts/setup/acoustic_inventory.py
+python scripts/process/strikes_find.py
+python scripts/process/strikes_save.py
+python scripts/process/strikes_corr.py
+python scripts/process/template_extraction.py
