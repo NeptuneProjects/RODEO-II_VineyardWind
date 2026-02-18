14412839ac6d13c0470027ea9d6222aba413ae5f
William Jenkins <wfjenkins@ucsd.edu>
2026-02-18 10:58:43 -0800

    Refactor scripts.

---
+++ scripts/acoustic_data_view.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Plot acoustic data for specific sensors and time intervals
+# Usage: ./acoustic_data_view.sh
+
+python ./scripts/plot/acoustic_data_view.py \
+    --sensor 3dvha \
+    --start "2023-12-01T21:00:00" \
+    --end "2023-12-01T23:30:00" \
+    --multi \
+    --interval 60 \
+    --savefig \
+    --target-fs 250.0 \
+    --filter highpass \
+    --filt-freq 10.0
+
+python ./scripts/plot/acoustic_data_view.py \
+    --sensor vla1 \
+    --start "2023-12-01T21:26:00" \
+    --end "2023-12-02T01:13:00" \
+    --multi \
+    --interval 60 \
+    --savefig \
+    --target-fs 250.0 \
+    --filter highpass \
+    --filt-freq 10.0
+
+python ./scripts/plot/acoustic_data_view.py \
+    --sensor vla2 \
+    --start "2023-12-01T21:44:00" \
+    --end "2023-12-02T01:31:00" \
+    --multi \
+    --interval 60 \
+    --savefig \
+    --target-fs 250.0 \
+    --filter highpass \
+    --filt-freq 10.0
+
+python ./scripts/plot/acoustic_data_view.py \
+    --sensor all \
+    --start "2023-12-01T21:44:00" \
+    --end "2023-12-01T23:30:00" \
+    --multi \
+    --interval 60 \
+    --savefig \
+    --target-fs 1000.0 \
+    --filter bandpass \
+    --filt-freq 100.0 350.0 \
+    --fmin 100.0 \
+    --fmax 350.0 \
